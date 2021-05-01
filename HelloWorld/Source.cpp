cc287b5f0eebcd46d00f978a1c5adab7fbc1a7aa
cstreit19 <colstonstreit@gmail.com>
2021-04-30 22:43:26 -0400

    Got rid of extraneous files.

--- HelloWorld/Source.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-	int x[5];
-
-	std::cout << "Hello World" << std::endl;
-	return 0;
-}
--- HelloWorld/Source1.cpp
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	std::cout << "Hello" << std::endl;
-}
