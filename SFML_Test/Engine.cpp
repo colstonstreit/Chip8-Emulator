3f5e25c701f0dda89d2136478a13d53ad3079598
cstreit19 <colstonstreit@gmail.com>
2021-06-02 11:47:41 -0400

    Organized things into header files and cpp files like I should have from the beginning. Everything should be done now!

---
+++ SFML_Test/Engine.cpp
@@ -0,0 +1,87 @@
+#include "Engine.h"
+
+Engine::Engine(const std::string& title, int width, int height, int scale = 1) :
+	renderer(width, height, scale),
+	window(sf::VideoMode(width* scale, height* scale), title, sf::Style::Close),
+	chip8(nullptr)
+{}
+
+Engine::~Engine() {}
+
+void Engine::handleEvents() {
+
+	sf::Event event;
+	while (window.pollEvent(event)) {
+
+		if (event.type == sf::Event::Closed)
+			window.close();
+
+		if (event.type == sf::Event::KeyPressed) {
+			chip8->setKey(getKeyCode(event.key.code), true);
+		}
+
+		if (event.type == sf::Event::KeyReleased) {
+			chip8->setKey(getKeyCode(event.key.code), false);
+			if (event.key.code == sf::Keyboard::Key::P) {
+				chip8->printStatus();
+			}
+		}
+
+	}
+}
+
+sf::Uint8 Engine::getKeyCode(sf::Keyboard::Key key) {
+
+	switch (key) {
+	case sf::Keyboard::Key::Num1:
+		return 0x1;
+	case sf::Keyboard::Key::Num2:
+		return 0x2;
+	case sf::Keyboard::Key::Num3:
+		return 0x3;
+	case sf::Keyboard::Key::Num4:
+		return 0xC;
+	case sf::Keyboard::Key::Q:
+		return 0x4;
+	case sf::Keyboard::Key::W:
+		return 0x5;
+	case sf::Keyboard::Key::E:
+		return 0x6;
+	case sf::Keyboard::Key::R:
+		return 0xD;
+	case sf::Keyboard::Key::A:
+		return 0x7;
+	case sf::Keyboard::Key::S:
+		return 0x8;
+	case sf::Keyboard::Key::D:
+		return 0x9;
+	case sf::Keyboard::Key::F:
+		return 0xE;
+	case sf::Keyboard::Key::Z:
+		return 0xA;
+	case sf::Keyboard::Key::X:
+		return 0x0;
+	case sf::Keyboard::Key::C:
+		return 0xB;
+	case sf::Keyboard::Key::V:
+		return 0xF;
+	default:
+		return -1;
+	}
+
+}
+
+void Engine::displayPixels() {
+	sf::Image image;
+	sf::Texture texture;
+	sf::Sprite sprite;
+
+	int width = renderer.getWidth(), height = renderer.getHeight(), scale = renderer.getScale();
+
+	image.create(width * scale, height * scale, renderer.getPixels());
+	texture.loadFromImage(image);
+	sprite.setTexture(texture);
+
+	window.draw(sprite);
+	window.display();
+}
---
+++ SFML_Test/Renderer.cpp
@@ -0,0 +1,50 @@
+#include "Renderer.h"
+
+Renderer::Renderer(int width, int height, int scale = 1) : width(width), height(height), scale(scale) {
+	byteArray = new sf::Uint8[width * height * scale * scale * 4];
+	memset(byteArray, 0, width * height * scale * scale * 4);
+}
+
+Renderer::~Renderer() {
+	delete[] byteArray;
+}
+
+sf::Uint32 Renderer::getPixel(int x, int y) {
+	int index = x * scale + y * width * scale * scale;
+	return (byteArray[index + 0] << 24) | (byteArray[index + 1] << 16) | (byteArray[index + 2] << 8) | byteArray[index + 3];
+}
+
+void Renderer::setPixel(int x, int y, sf::Uint32 color) {
+
+	if (isValidCoordinate(x, y)) {
+
+		int effectiveWidth = width * scale;
+
+		for (int i = 0; i < scale; i++) {
+			for (int j = 0; j < scale; j++) {
+				int index = (x * scale + y * effectiveWidth * scale + i * effectiveWidth + j) * 4;
+				byteArray[index + 0] = (color & 0xFF000000) >> 24;
+				byteArray[index + 1] = (color & 0x00FF0000) >> 16;
+				byteArray[index + 2] = (color & 0x0000FF00) >> 8;
+				byteArray[index + 3] = (color & 0x000000FF);
+			}
+		}
+
+	}
+}
+
+void Renderer::clearDisplay(sf::Uint32 color = 0x000000FF) {
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			setPixel(x, y, color);
+		}
+	}
+}
+
+void Renderer::fillRect(int x1, int y1, int width, int height, sf::Uint32 color) {
+	for (int y = y1; y < y1 + height; y++) {
+		for (int x = x1; x < x1 + width; x++) {
+			setPixel(x, y, color);
+		}
+	}
+}
